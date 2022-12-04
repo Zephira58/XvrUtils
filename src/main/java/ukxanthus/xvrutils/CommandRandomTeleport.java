262e77e1ab61c45c7b11bb4e50e972c355f33308
Xanthus <Xanthus58@protonmail.com>
2022-12-05 02:41:40 +1030

    Add: java properties Fix: Legecy RTP Code

--- src/main/java/ukxanthus/xvrutils/CommandRandomTeleport.java
+++ src/main/java/ukxanthus/xvrutils/CommandRandomTeleport.java
@@ -7,38 +7,36 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.Properties;
 import java.util.Random;
 import java.util.logging.*;
 
 public class CommandRandomTeleport implements CommandExecutor {
+    Properties props = new Properties();
     Logger logger = Logger.getLogger(XvrUtils.class.getName());
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        //Detect if the sender is a player
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            //Get the player's current location
             Location location = player.getLocation();
-            //Get the world the player is in
             World world = player.getWorld();
-            //Generate a random number between 0 and 1000
+
             Random random = new Random();
-            //Assign y a random number between 50 and 1000
-            int y = random.nextInt(1000) + 50;
             int x = random.nextInt(4000);
             int z = random.nextInt(4000) - 7304;
-            //Set the player's location to the random location
+
             location.setX(x);
             location.setY(85);
             location.setZ(z);
+
             player.teleport(location);
-            //Send the player a message
             player.sendMessage(ChatColor.GREEN + "[XVR Utils]" + " You have been teleported to " + x + ", " + z);
-            logger.info("[XVR Utils] " + sender.getName() + " has used the /rtp command! Teleporting to " + x + ", " + z);
+            logger.info(props.get("PREFIX") + sender.getName() + " has used the /rtp command! Teleporting to " + x + ", " + z);
             return true;
+
         } else {
-            //If the sender is not a player, send a message
             sender.sendMessage(ChatColor.GREEN + "[XVR Utils]" + ChatColor.RED + "You must be a player to use this command!");
-            logger.info("[XVR Utils] A non-player has tried to use the /rtp command!");
+            logger.info(props.get("PREFIX") + " A non-player has tried to use the /rtp command!");
             return false;
         }
     }
--- src/main/java/ukxanthus/xvrutils/CommandSpawn.java
+++ src/main/java/ukxanthus/xvrutils/CommandSpawn.java
@@ -6,20 +6,22 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.command.CommandSender;
 
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class CommandSpawn implements CommandExecutor {
+    Properties props = new Properties();
     Logger logger = Logger.getLogger(XvrUtils.class.getName());
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
                 player.teleport(player.getWorld().getSpawnLocation());
                 player.sendMessage(ChatColor.GREEN + "[XVR Utils]" + " You have been teleported to spawn!");
-                logger.info("[XVR Utils] " + sender.getName() + " has used the /spawn command! Teleporting to spawn...");
+                logger.info(props.get("PREFIX") + sender.getName() + "has used the /spawn command! Teleporting to spawn...");
                 return true;
             } else {
                 sender.sendMessage("You must be a player to use this command!");
-                logger.info("[XVR Utils] A non-player has tried to use the /spawn command!");
+                logger.info(props.get("PREFIX") + " A non-player has tried to use the /spawn command!");
                 return true;
             }
     }
